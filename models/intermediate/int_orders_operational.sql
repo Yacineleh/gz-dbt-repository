4b34f0bb2897db6b6214073e5a320078d4e16f7d
Yacine Lehouali <yacinelehouali1996@gmail.com>
2024-05-07 14:04:02 +0000

    update ops cost

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,5 @@
 SELECT 
-margeee.orders_id,
-margeee.date_date,
+margeee.*,
 margeee.margin+CAST(shippp.shipping_fee AS float64)-CAST(logcost AS float64)-CAST(ship_cost AS float64) AS operational_cost
 FROM {{ ref('int_orders_margin') }} as margeee
 LEFT JOIN {{ ref('stg_raw__ship') }} AS shippp
