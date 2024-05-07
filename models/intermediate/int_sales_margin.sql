69bf967ba06aaacd6b0b122085a7970896ed952f
Yacine Lehouali <yacinelehouali1996@gmail.com>
2024-05-07 15:11:49 +0000

    final project

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -10,7 +10,8 @@ SELECT
 sales.*, 
 product.purchSE_PRICE AS purchase_price,
 CAST(quantity AS float64)*CAST(purchSE_PRICE AS float64) AS Purchase_cost,
-CAST(revenue AS float64)-CAST(quantity AS float64)*CAST(purchSE_PRICE AS float64) AS margin
+CAST(revenue AS float64)-CAST(quantity AS float64)*CAST(purchSE_PRICE AS float64) AS margin,
+CONCAT(orders_id,"_",pdt_id) AS primary_key
 FROM sales as sales
 LEFT JOIN product 
 ON sales.pdt_id=product.products_id
