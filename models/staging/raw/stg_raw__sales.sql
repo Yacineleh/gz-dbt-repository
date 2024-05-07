7ad69bff41977511d6803177d0f210e6f754959f
Yacine Lehouali <yacinelehouali1996@gmail.com>
2024-05-07 12:15:32 +0000

    new update

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -13,7 +13,8 @@ renamed as (
         orders_id,
         pdt_id AS products_id,
         revenue,
-        quantity
+        quantity,
+        concat(orders_id,"_", pdt_id) AS primary_key
 
     from source
 
